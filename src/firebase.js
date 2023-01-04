c2533ca58be97fcbf9aa05e0d9a56f1649fec79d
Jason Soukchamroeun <j.soukchamroeun@gmail.com>
2023-01-04 13:14:20 -0800

    Firebase - Setup

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyC-bLDuRMWNREpwPZC1QJ5cC-wj_f0nvS8",
-    authDomain: "clone-build-8ade1.firebaseapp.com",
-    projectId: "clone-build-8ade1",
-    storageBucket: "clone-build-8ade1.appspot.com",
-    messagingSenderId: "739088183592",
-    appId: "1:739088183592:web:534b0550a295a9b6f9201d"
-  };
